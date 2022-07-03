83648dfbd815e2c69e18fda5b0bc38f961370e4b
DeltaF1 <delyaerricson@gmail.com>
2022-07-03 02:38:54 -0400

    Skip header bytes

--- src/main.rs
+++ src/main.rs
@@ -108,7 +108,9 @@ fn decode(qoi: &[u8]) -> Result<Vec<u8>, QOIError> {
     }; 64];
     let mut output = vec![];
 
-    let mut iter = qoi.iter();
+    let mut iter = qoi
+        .iter()
+        .skip(14); // Skip over the header for now
 
     while let Some(qoi_byte) = iter.next() {
         let op: Op = (*qoi_byte).into();
